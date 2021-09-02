262f765f0603c3b18d2dc5b87be948b319d05e80
gopal1860 <78166558+gopal1860@users.noreply.github.com>
2021-09-02 19:02:38 +0530

    #1
    uploded

---
+++ main.py
@@ -0,0 +1,27 @@
+import speech_recognition as sr
+import pyttsx3
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.say("hello buddy am the wight commander")
+    engine.runAndWait()
+
+
+try:
+    with sr.Microphone() as source:
+        print('listening...')
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        command = command.lower()
+        if 'alexa' in command:
+            command = command.replace('alexa', '')
+            print(command)
+            talk(command)
+except:
+    pass
