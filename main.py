ec509dea13b33db840900b857174234ff592fac6
gopal1860 <78166558+gopal1860@users.noreply.github.com>
2021-11-18 18:44:31 +0530

    update time
    time Added

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
+import datetime
+
+import wikipedia
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
 
 
 def talk(text):
     engine.say(text)
-    engine.say("hello buddy am the wight commander")
+    engine.say(text)
     engine.runAndWait()
 
 
-try:
-    with sr.Microphone() as source:
-        print('listening...')
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        command = command.lower()
-        if 'alexa' in command:
-            command = command.replace('alexa', '')
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print("listening....")
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if "commander" in command:
+                command = command.replace("commander", " ")
+                print(command)
+    except:
+        talk("hi am wight commander give me a command then i answer you we can make conversatiton")
+        print("hi am wight commander give me a command then i answer you we can make conversatiton")
+        pass
+    return command
+
+
+def run_commander():
+            command = take_command()
             print(command)
-            talk(command)
-except:
-    pass
+            if 'play' in command: 
+                song = command.replace("play", " ")
+                talk("playing" + song)
+                pywhatkit.playonyt(song)
+            elif 'time' in command:
+                time = datetime.datetime.now().strftime('%I:%M %p')
+                talk( "the time is" + time)
+                print(time)
+            elif 'search' in command:
+                search = command.replace('search', "")
+                Ans = wikipedia.summary(search,1)
+                talk(Ans)
+                print(Ans)
+            else:
+                talk("i can not understand your words buddy , can say again clearly")
+                print("i can not understand your words buddy , can say again clearly")
+
+run_commander()
